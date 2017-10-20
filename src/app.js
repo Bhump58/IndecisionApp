fe8f00e94beabb9caa3962bddda31fa4e4804869
Brian Humphrey <Humphreybrian58@gmail.com>
2017-10-20 16:55:17 -0400

    Webpack installation

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log('app.js is running');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const template = React.createElement('h1', {}, 'testing');
+const app = document.getElementById('app');
+ReactDom.render(template, app);
